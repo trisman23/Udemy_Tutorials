b8f9f3692f25584a63bf0867c73bbc40a502188d
DESKTOP-LSLCNE7\Tris <tristanlow@googlemail.com>
2017-11-20 10:36:35 +0000

    BE07 Accessing Object Names

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "Gameframework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -19,7 +20,8 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on Chair!"));
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report for %s!"), *ObjectName);
 	
 }
 
